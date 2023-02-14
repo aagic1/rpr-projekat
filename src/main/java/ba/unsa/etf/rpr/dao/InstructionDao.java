1df1fe15447f251785322c1d577f5e325ae153bb
aagic1 <aagic1@etf.unsa.ba>
2023-02-14 21:20:45 +0100

    Create dao interface for Instruction domain bean

---
+++ src/main/java/ba/unsa/etf/rpr/dao/InstructionDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Instruction;
+
+/**
+ * Dao interface for Instruction domain bean
+ */
+public interface InstructionDao extends Dao<Instruction> {
+
+}
