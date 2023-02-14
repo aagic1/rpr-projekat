4c5c16c22ed3dcb40a4564d7822a2b61c2736237
aagic1 <aagic1@etf.unsa.ba>
2023-02-14 21:24:09 +0100

    Implement row2object method for Instruction

--- src/main/java/ba/unsa/etf/rpr/dao/InstructionDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/InstructionDaoSQLImpl.java
@@ -3,6 +3,7 @@ package ba.unsa.etf.rpr.dao;
 import ba.unsa.etf.rpr.domain.Instruction;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 /**
@@ -16,7 +17,14 @@ public class InstructionDaoSQLImpl extends AbstractDao<Instruction> implements I
 
     @Override
     public Instruction row2object(ResultSet rs) {
-        return null;
+        Instruction instruction = new Instruction();
+        try {
+            instruction.setId(rs.getInt("id"));
+            instruction.setStep(rs.getInt("step"));
+            instruction.setDescription(rs.getString("description"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
