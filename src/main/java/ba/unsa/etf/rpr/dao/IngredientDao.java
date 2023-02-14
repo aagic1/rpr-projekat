3338ecbf9b794054e71d2e1896388269fe6e4b64
aagic1 <aagic1@etf.unsa.ba>
2023-02-14 21:00:01 +0100

    Create interface for Ingredient domain bean

---
+++ src/main/java/ba/unsa/etf/rpr/dao/IngredientDao.java
@@ -0,0 +1,10 @@
+package ba.unsa.etf.rpr.dao;
+
+import ba.unsa.etf.rpr.domain.Ingredient;
+
+/**
+ * Dao interface for Ingredient domain bean
+ */
+public interface IngredientDao extends Dao<Ingredient> {
+
+}
