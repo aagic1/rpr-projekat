84da1c990e695c37ddcdcaa15b3b75d787900e8d
aagic1 <aagic1@etf.unsa.ba>
2023-02-14 21:54:36 +0100

    Create java bean for User

---
+++ src/main/java/ba/unsa/etf/rpr/domain/User.java
@@ -0,0 +1,75 @@
+package ba.unsa.etf.rpr.domain;
+
+import java.util.Objects;
+
+/**
+ * Java bean for User
+ */
+public class User implements Idable{
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void setId(int id) {
+
+    }
+
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && Objects.equals(username, user.username) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return username;
+    }
+}
