f7d14310f1d643298b7614b22bb78fa02db62706
aagic1 <aagic1@etf.unsa.ba>
2023-02-15 12:08:08 +0100

    Implement row2object method for Recipe

--- src/main/java/ba/unsa/etf/rpr/dao/RecipeDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/RecipeDaoSQLImpl.java
@@ -2,7 +2,9 @@ package ba.unsa.etf.rpr.dao;
 
 import ba.unsa.etf.rpr.domain.Recipe;
 
+import java.sql.Date;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Map;
 
 public class RecipeDaoSQLImpl extends AbstractDao<Recipe> implements RecipeDao {
@@ -27,7 +29,20 @@ public class RecipeDaoSQLImpl extends AbstractDao<Recipe> implements RecipeDao {
 
     @Override
     public Recipe row2object(ResultSet rs) {
-        return null;
+        Recipe recipe = new Recipe();
+        try {
+            recipe.setId(rs.getInt("id"));
+            recipe.setTitle(rs.getString("title"));
+            recipe.setDate(rs.getDate("date").toLocalDate());
+            recipe.setDescription(rs.getString("description"));
+            recipe.setPreparationTime(rs.getInt("preparation_time"));
+            recipe.setCookTime(rs.getInt("cook_time"));
+            recipe.setServings(rs.getInt("servings"));
+            recipe.setNotes(rs.getString("notes"));
+            return recipe;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
