b5d0418153224815bce09ef0351f7d42daf3b8de
aagic1 <aagic1@etf.unsa.ba>
2023-02-16 16:27:37 +0100

    Open signup window on mouse click

--- src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -4,7 +4,16 @@ import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.domain.User;
 import ba.unsa.etf.rpr.exception.RecipeException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Region;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -49,5 +58,21 @@ public class LoginController {
             lblValidationEmail.setText("Email is not linked with an account.");
         }
 
+    }
+
+    public void actionSignup(MouseEvent mouseEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/signup.fxml"));
+            loader.setController(new SignUpController());
+            Stage stage = new Stage();
+            stage.setScene(new Scene(loader.load(), Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+            stage.setTitle("SignUp");
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner((Stage)fldEmail.getScene().getWindow());
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
     }
 }
