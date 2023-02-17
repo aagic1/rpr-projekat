ec2278bafe155235fd95b0141997bb220b3297eb
aagic1 <aagic1@etf.unsa.ba>
2023-02-17 19:18:11 +0100

    Implement add step button functionality

--- src/main/java/ba/unsa/etf/rpr/controllers/RecipeFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RecipeFormController.java
@@ -3,12 +3,16 @@ package ba.unsa.etf.rpr.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 
 public class RecipeFormController {
     public VBox boxIngredients;
+    public VBox boxInstructions;
 
     public void addIngredient(ActionEvent actionEvent) {
         try {
@@ -16,6 +20,19 @@ public class RecipeFormController {
             loader.setController(new IngredientTemplateController(boxIngredients));
             Node node = loader.load();
             boxIngredients.getChildren().add(node);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addStep(ActionEvent actionEvent) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/stepTemplate.fxml"));
+            loader.setController(new StepTemplateController());
+            HBox hbox = loader.load();
+            Label number = (Label) hbox.getChildren().get(0);
+            number.setText((boxInstructions.getChildren().size() + 1) + ".");
+            boxInstructions.getChildren().add(hbox);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
