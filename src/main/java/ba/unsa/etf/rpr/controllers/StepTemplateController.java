4827b78674154f44b214e11bc98cea07dbe6acb9
aagic1 <aagic1@etf.unsa.ba>
2023-02-17 19:14:47 +0100

    Create step template for adding steps to recipe

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/StepTemplateController.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+
+public class StepTemplateController {
+    public StepTemplateController() {
+    }
+
+    public void deleteStep(ActionEvent actionEvent) {
+
+    }
+}
