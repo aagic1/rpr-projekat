f4e3a83bce1f344845cc464f22f858b4a0cb51ea
aagic1 <aagic1@etf.unsa.ba>
2023-02-17 19:24:58 +0100

    Delete step on button click

--- src/main/java/ba/unsa/etf/rpr/controllers/StepTemplateController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/StepTemplateController.java
@@ -1,12 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class StepTemplateController {
-    public StepTemplateController() {
+    RecipeFormController recipeFormController;
+    public StepTemplateController(RecipeFormController recipeFormController) {
+        this.recipeFormController = recipeFormController;
     }
 
     public void deleteStep(ActionEvent actionEvent) {
-
+        Button button = (Button) actionEvent.getSource();
+        HBox step = (HBox) button.getParent();
+        recipeFormController.removeStep(step);
     }
 }
