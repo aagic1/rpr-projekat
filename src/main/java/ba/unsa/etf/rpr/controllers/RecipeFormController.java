1e5e9147bbfccb6c586c628480e430c779aa0033
aagic1 <aagic1@etf.unsa.ba>
2023-02-17 23:05:26 +0100

    Add spinners for specifiying time and servings

--- src/main/java/ba/unsa/etf/rpr/controllers/RecipeFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RecipeFormController.java
@@ -1,17 +1,48 @@
 package ba.unsa.etf.rpr.controllers;
 
+import com.mysql.cj.result.IntegerValueFactory;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class RecipeFormController {
     public VBox boxIngredients;
     public VBox boxInstructions;
+    public Spinner prepTimeHours;
+    public Spinner cookTimeHours;
+    public Spinner prepTimeMinutes;
+    public Spinner cookTimeMinutes;
+    public Spinner servings;
+
+    @FXML
+    public void initialize() {
+        initSpinnerMinutes(prepTimeMinutes);
+        initSpinnerMinutes(cookTimeMinutes);
+        initSpinnerHours(prepTimeHours);
+        initSpinnerHours(cookTimeHours);
+        initSpinnerServings(servings);
+    }
+
+    private void initSpinnerMinutes(Spinner spinner) {
+        spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 59));
+    }
+
+    private void initSpinnerHours(Spinner spinner) {
+        spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 23));
+    }
+
+    private void initSpinnerServings(Spinner spinner) {
+        spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 999));
+    }
 
     public void addIngredient(ActionEvent actionEvent) {
         try {
@@ -50,5 +81,9 @@ public class RecipeFormController {
             lblNumber.setText(counter + ".");
             counter++;
         }
+    }
+
+    public void actionCancel(ActionEvent actionEvent) {
+        ((Stage) boxInstructions.getScene().getWindow()).close();
     }
 }
