a3ed1cbb963b7a0c3dd7402231a9d13c8208a46c
aagic1 <aagic1@etf.unsa.ba>
2023-02-17 15:36:40 +0100

    Open recipe creation form on button click

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,6 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.Region;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeController {
     private static User loggedInUser = null;
@@ -13,4 +25,25 @@ public class HomeController {
     public void logOutUser() {
         loggedInUser = null;
     }
+
+
+    public void openCreateRecipe(ActionEvent actionEvent) {
+        try {
+            Node node = ((Node) actionEvent.getSource());
+            Stage parentStage = ((Stage) node.getScene().getWindow());
+
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/recipeForm.fxml"));
+            Parent root = loader.load();
+            stage.setScene(new Scene(root, Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE));
+            stage.setTitle("Create recipe");
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(parentStage);
+            stage.show();
+        } catch (IOException e) {
+            new Alert(Alert.AlertType.NONE, e.getMessage(), ButtonType.OK).show();
+        }
+    }
+
+
 }
