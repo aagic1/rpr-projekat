c7ccfc45dfa9f83c37396aaa26a7fff98ed27fc2
aagic1 <aagic1@etf.unsa.ba>
2023-02-20 18:01:42 +0100

    Display ingredients and instructions when opening recipe

--- src/main/java/ba/unsa/etf/rpr/controllers/RecipeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RecipeController.java
@@ -1,8 +1,19 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.IngredientManager;
+import ba.unsa.etf.rpr.business.InstructionManager;
+import ba.unsa.etf.rpr.domain.Ingredient;
+import ba.unsa.etf.rpr.domain.Instruction;
 import ba.unsa.etf.rpr.domain.Recipe;
+import ba.unsa.etf.rpr.exception.RecipeException;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.util.List;
 
 public class RecipeController {
     public Label lblTitle;
@@ -13,7 +24,11 @@ public class RecipeController {
     public Label lblTotalTime;
     public Label lblServings;
     public Label lblNotes;
+    public VBox boxIngredients;
+    public VBox boxInstructions;
     private Recipe recipe;
+    private IngredientManager ingredientManager = new IngredientManager();
+    private InstructionManager instructionManager = new InstructionManager();
 
     public RecipeController(Recipe recipe) {
         this.recipe = recipe;
@@ -22,8 +37,56 @@ public class RecipeController {
     @FXML
     public void initialize() {
         populateRecipe();
+        populateIngredients();
+        populateInstructions();
     }
 
+    private void populateInstructions() {
+        try {
+            List<Instruction> instructions = instructionManager.getInstructionsByRecipe(recipe);
+            for (int i = 0; i < instructions.size(); i++) {
+                HBox box = new HBox();
+                Label lblNumber = new Label("" + (i+1) + ".\t");
+                Label lblText = new Label(instructions.get(i).getDescription());
+                lblText.setWrapText(true);
+                lblText.prefWidth(640);
+                lblText.setMaxWidth(640);
+                box.getChildren().add(lblNumber);
+                box.getChildren().add(lblText);
+                boxInstructions.getChildren().add(box);
+            }
+        } catch (RecipeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void populateIngredients() {
+        try {
+            List<Ingredient> ingredients = ingredientManager.getIngredientsByRecipe(recipe);
+            for (int i = 0; i < ingredients.size(); i++) {
+                Label lbl = new Label(formatIngredient(ingredients.get(i)));
+                lbl.setWrapText(true);
+                lbl.prefWidth(640);
+                lbl.setMaxWidth(640);
+                boxIngredients.getChildren().add(lbl);
+            }
+        } catch (RecipeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String formatIngredient(Ingredient ingredient) {
+        String text = "\t- ";
+        if (ingredient.getAmount() != null && !ingredient.getAmount().isBlank()) {
+            text += ingredient.getAmount() + " ";
+        }
+        if (ingredient.getMeasurementUnit() != null && !ingredient.getMeasurementUnit().isBlank()) {
+            text += ingredient.getMeasurementUnit() + " ";
+        }
+        text += ingredient.getName();
+        return text;
+    }
+    
     private void populateRecipe() {
         lblTitle.setText(recipe.getTitle());
         lblDescription.setText(recipe.getDescription());
