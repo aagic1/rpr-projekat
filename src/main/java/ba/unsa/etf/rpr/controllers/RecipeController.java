1c6bef43daa2133afa8dc2cf3e77fcf4a25f0146
aagic1 <aagic1@etf.unsa.ba>
2023-02-20 16:28:17 +0100

    Populate recipe data into recipe window

--- src/main/java/ba/unsa/etf/rpr/controllers/RecipeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RecipeController.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr.controllers;
 
 import ba.unsa.etf.rpr.domain.Recipe;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class RecipeController {
@@ -17,6 +18,34 @@ public class RecipeController {
     public RecipeController(Recipe recipe) {
         this.recipe = recipe;
     }
-    
-    
+
+    @FXML
+    public void initialize() {
+        populateRecipe();
+    }
+
+    private void populateRecipe() {
+        lblTitle.setText(recipe.getTitle());
+        lblDescription.setText(recipe.getDescription());
+        lblUsername.setText(recipe.getOwner().getUsername());
+        lblNotes.setText(recipe.getNotes());
+        lblServings.setText("" + recipe.getServings());
+        lblPreparationTime.setText(getTimeHoursMinutes(recipe.getPreparationTime()));
+        lblCookTime.setText(getTimeHoursMinutes(recipe.getCookTime()));
+        lblTotalTime.setText(getTimeHoursMinutes(recipe.getPreparationTime() + recipe.getCookTime()));
+    }
+
+    private String getTimeHoursMinutes(int time) {
+        int hours = time / 60;
+        int minutes = time % 60;
+        String text = "";
+        if (hours > 0) {
+            text += hours + "h ";
+        }
+        if (minutes > 0) {
+            text += minutes + "min";
+        }
+        return text;
+    }
+
 }
